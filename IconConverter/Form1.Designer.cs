4701d6099f2e4d28489ab19fe468d8cd7deb6a72
Timo <Timo@TIMO-PC>
2022-11-21 15:19:45 +0100

    Projektdateien hinzufügen.

---
+++ IconConverter/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace IconConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(308, 77);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start Converting";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "No Convert Done";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "No Convert Done";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 166);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ IconConverter/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IconConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = $"Start Convert {DateTime.Now}.";
+
+
+            var convertResult = this.Convert();
+            if (convertResult != null && convertResult.HasValue)
+            {
+                if (convertResult.Value)
+                    label2.Text = $"End Convert {DateTime.Now}.";
+                else
+                    label2.Text = $"Error while converting.";
+            }
+            else
+                label2.Text = "Convert Canceled.";
+        }
+
+        private bool? Convert()
+        {
+            try
+            {
+                var filePath = string.Empty;
+
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.InitialDirectory = "c:\\";
+                    openFileDialog.Filter = "All Graphics Types|*.jpg;*.jpeg;*.png;";
+                    openFileDialog.FilterIndex = 2;
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        var fileinfo = new FileInfo(openFileDialog.FileName);
+
+                        filePath = fileinfo.Directory.FullName;
+
+                        var result = Path.ChangeExtension(openFileDialog.FileName, ".ico");
+
+                        using (FileStream FS = File.OpenWrite(result))
+                        {
+
+                            Bitmap bitmap = (Bitmap)Image.FromFile(openFileDialog.FileName);
+                            Icon.FromHandle(bitmap.GetHicon()).Save(FS);
+                        }
+
+                        MessageBox.Show($"Your File is ready on :{filePath}");
+                    }
+                    else
+                        return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
